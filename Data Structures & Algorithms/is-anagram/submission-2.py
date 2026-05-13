af347babbedd3975d723299553efba6145803902
Andrei <amrusu2@gmail.com>
2026-05-13 23:40:05 +0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq = {}
+
+        for char in s:
+            if char not in freq:
+                freq[char] = 1
+            else:
+                freq[char] += 1
+
+        for char in t:
+            if char not in freq:
+                freq[char] = 1
+            else:
+                freq[char] -= 1
+        
+        for value in freq.values():
+            if value != 0:
+                return False
+        
+        return True
+        
+
+
+        
